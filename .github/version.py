2ba75fb3dd95bf06b59da9ed6995c64e28f289e6
MistEO <mistereo@hotmail.com>
2025-01-16 17:30:35 +0800

    Update version.py

--- .github/version.py
+++ .github/version.py
@@ -5,7 +5,8 @@ if len(sys.argv) <= 1:
   exit(1)
 
 version_file = sys.argv[1]
-j = json.load(version_file)
+with open('version_file', 'r') as file:
+  j = json.load(file)
 
 if j["activity"]["time"] < j["gacha"]["time"]:
   display = j["gacha"]["pool"]
